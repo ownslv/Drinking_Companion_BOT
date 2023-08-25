3165ff351735ddbe2ff0424699d5410c7a84200e
ownslv <owning.zanji@yandex.ru>
2023-08-25 10:35:40 +0300

    Добавил функцию тригера на всех юзеров

--- zxc.py
+++ zxc.py
@@ -13,12 +13,25 @@ bot = telebot.TeleBot(env.str("TOKEN"))
 
 @bot.message_handler(content_types=['text'])
 def send_message_user(message):
+    """Тригер на всех"""
     random.shuffle(tlist)
-    drunkard = env.int('DRUNKARD')
-    if message.from_user.id == drunkard:
-        bot.reply_to(message, random.choice(tlist))
-    else:
-        None
+    bot.reply_to(message, random.choice(tlist))
+
+
+# Если хочешь использовать тригер на конкретного юзера
+# Раскомментируй функцию ниже
+# А функцию выше наоборт закомментируй
+
+
+# @bot.message_handler(content_types=['text'])
+# def send_message_user(message):
+#     """Тригер на конкретного юзера"""
+#     random.shuffle(tlist)
+#     drunkard = env.int('DRUNKARD')
+#     if message.from_user.id == drunkard:
+#         bot.reply_to(message, random.choice(tlist))
+#     else:
+#         None
 
 
 bot.polling(none_stop=True)
