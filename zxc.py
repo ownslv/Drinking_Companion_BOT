27f2e39e91c3f3644c15a7ccce5a0175f07f0e8d
ownslv <owning.zanji@yandex.ru>
2023-08-26 22:35:02 +0300

    Добавил команды

--- zxc.py
+++ zxc.py
@@ -3,25 +3,73 @@ import random
 import telebot
 from environs import Env
 
-from list import tlist
 from ingeenegr import inglist
+from list import tlist
 
 env = Env()
 env.read_env()
 
-bot = telebot.TeleBot(env.str("TOKEN"))
+bot = telebot.TeleBot(env.str('FAST'))
+
+status = 0
+black_list = []
+print(status)
+print(black_list)
 
 
-# @bot.message_handler(content_types=['text'])
-# def send_message_user(message):
-#     """Тригер на всех"""
-#     random.shuffle(tlist)
-#     bot.reply_to(message, random.choice(tlist))
+def check_status(user_id):
+    global status
+    if status == 0:
+        return False
+    if user_id in black_list:
+        return False
+    return True
 
 
-# Если хочешь использовать тригер на конкретного юзера
-# Раскомментируй функцию ниже
-# А функцию выше наоборт закомментируй
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    admin = env.int('ADMIN')
+    if message.from_user.id == admin:
+        global status
+        status = 1
+        bot.send_message(message.chat.id, 'Всем общий салам')
+    else:
+        None
+
+
+@bot.message_handler(commands=['stop'])
+def stop_message(message):
+    admin = env.int('ADMIN')
+    if message.from_user.id == admin:
+        global status
+        status = 0
+        bot.send_message(message.chat.id, 'Всем общий досвидос')
+    else:
+        bot.send_message(message.chat.id, 'хд наивный глупец')
+
+
+@bot.message_handler(commands=['block'])
+def block_user(message):
+    global black_list
+    id_user = message.from_user.id
+    if id_user in black_list:
+        bot.reply_to(message, 'Ты уже в чс')
+    else:
+        black_list.append(id_user)
+        bot.reply_to(message, 'Это бан')
+        print(black_list)
+
+
+@bot.message_handler(commands=['unblock'])
+def unblock_user(message):
+    global black_list
+    id_user = message.from_user.id
+    if id_user in black_list:
+        black_list.remove(id_user)
+        bot.reply_to(message, 'Добро пожаловать на сервер безумные зомби')
+        print(black_list)
+    else:
+        bot.reply_to(message, 'Ты и не был в бане, курва пердоле')
 
 
 @bot.message_handler(content_types=['text'])
@@ -29,30 +77,9 @@ def send_message_user(message):
     """Тригер на всех, кроме инжей"""
     random.shuffle(tlist)
     random.shuffle(inglist)
-    ARA = env.int('ARA')
-    VADIM = env.int('VADIM')
-    HOHOL = env.int('HOHOL')
-    if message.from_user.id == ARA or VADIM or HOHOL:
-        bot.reply_to(message, random.choice(inglist))
-    else:
-        None
-    bot.reply_to(message, random.choice(tlist))
-
-
-# Если хочешь использовать тригер на конкретного юзера
-# Раскомментируй функцию ниже
-# А функцию выше наоборт закомментируй
-
-
-# @bot.message_handler(content_types=['text'])
-# def send_message_user(message):
-#     """Тригер на конкретного юзера"""
-#     random.shuffle(tlist)
-#     drunkard = env.int('DRUNKARD')
-#     if message.from_user.id == drunkard:
-#         bot.reply_to(message, random.choice(tlist))
-#     else:
-#         None
+    user_id = message.from_user.id
+    if check_status(user_id):
+        bot.reply_to(message, random.choice(tlist))
 
 
 bot.polling(none_stop=True)
